d912caeb4041239129e339bb33f65596b5bd114b
Mirii <marvinrnr@gmail.com>
2019-11-28 11:25:23 +0100

    Update et fin de l'exercice 2

--- exercice2/src/com/company/FicReader.java
+++ exercice2/src/com/company/FicReader.java
@@ -1,25 +1,46 @@
 package com.company;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 
 public class FicReader {
 
-    public  FicReader() {
+    public FicReader() {
 
     }
-    public HashMap ReadFic(String ficName) throws IOException {
+
+    public HashMap ReadFic() throws IOException {
         HashMap<String,Integer>  ficContent = new HashMap<>() ;
-        BufferedReader reader = new BufferedReader(new FileReader(ficName));
+        BufferedReader reader = new BufferedReader(new FileReader("test.txt"));
         String line = null;
         while((line = reader.readLine()) != null){
             String[] lineTab = line.split(":");
             Integer score = Integer.parseInt(lineTab[1]);
             ficContent.put(lineTab[0],score) ;
-            System.out.print("Score : " +  ficContent.get(lineTab[0]) + "\n");
+            //       System.out.print("Score : " +  ficContent.get(lineTab[0]) + "\n");
 
         }
         return  ficContent;
+    }
+
+    public double getAverage(HashMap scoreStock) {
+        double result = 0;
+        int count = 0;
+        Collection values = scoreStock.values();
+        ArrayList<Integer> ListOfScores = new ArrayList<Integer>(values);
+        for (Integer value : ListOfScores) {
+            result += value;
+            count++;
+        }
+        result /= count;
+        return result;
+    }
+
+    public Integer getScore(HashMap student, String mail) {
+        return (Integer) student.get(mail);
     }
 }
--- exercice2/src/com/company/Main.java
+++ exercice2/src/com/company/Main.java
@@ -1,11 +1,15 @@
 package com.company;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 public class Main {
 
     public  static void main(String [] args ) throws IOException {
+        HashMap map = new HashMap<>();
         FicReader fc = new FicReader();
-        fc.ReadFic("test.txt");
+        map = fc.ReadFic();
+        System.out.print("Voici le score de l'etudiant marvin@gmail.com  " + fc.getScore(map, "marvin@gmail.com") + "\n");
+        System.out.print("Moyenne des scores : " + fc.getAverage(map));
     }
 }
