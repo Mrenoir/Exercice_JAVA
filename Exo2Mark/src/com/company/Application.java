77d13887261b0414dda8f00a68d9ce3040008fd3
Mirii <marvinrnr@gmail.com>
2020-02-10 17:01:00 +0100

    Exercice des De Java Avancé

---
+++ Exo2Mark/src/com/company/Application.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Application {
+    private String name;
+    private Student Students[];
+
+    public Application( String name  , Student[] students ) {
+        this.name = name;
+        Students = students;
+    }
+
+}
